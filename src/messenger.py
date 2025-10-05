50d5050acab55d2b239952b85801748fcb82ac75
Vishal Naik <vishal.naik@mercedes-benz.com>
2025-10-05 14:43:21 +0530

    new release - Sun 10/05/2025 14:43:21.06

--- src/messenger.py
+++ src/messenger.py
@@ -18,6 +18,9 @@ class WhatsAppMessenger:
         options.add_argument("--start-minimized")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--remote-debugging-port=9222")
 
         self.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
